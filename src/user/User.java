292d91766dccdf9c99b5382ebc8218711b5c8458
wsperandio <Werner.Sperandio@stud-inf.unibz.it>
2016-12-06 23:34:06 +0100

    add class User

--- src/user/User.java
+++ src/user/User.java
@@ -2,53 +2,106 @@ package user;
 
 import java.lang.reflect.Field;
 
+import javax.persistence.*;
+import org.hibernate.validator.constraints.*;
+
+@Entity
+@Table(name = "Employee")
 public class User {
 
-	String email;
-	String password;
-	int points;
-	String billingAdress;
-	String phoneNumber;
-	boolean vipStatus;
+    @Id
+    @SequenceGenerator(name = "user_seq", sequenceName = "seq_user")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
+    @Column(name = "id")
+    private Long userId;
+    
+    @NotBlank
+    @Column(name = "nick_name", unique = true, nullable = false)
+	private String nickName;
 	
+    @Email
+    @Column(name = "email")
+	private String email;
+    
+    @Column(name = "password") 	
+	private String password;
 	
+    @Column(name = "points") 	
+	private int points;
+	
+    @Column(name = "billing_adress")
+	private String billingAdress;
+	
+    @Column(name = "phone_number")
+	private String phoneNumber;
+	
+    @Column(name = "vip_status")
+	private boolean vipStatus;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "gender", nullable = false)
+	private GenderType gender;
+
+	public enum GenderType {
+		female, male
+	};
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public int getPoints() {
 		return points;
 	}
+
 	public void setPoints(int points) {
 		this.points = points;
 	}
+
 	public String getBillingAdress() {
 		return billingAdress;
 	}
+
 	public void setBillingAdress(String billingAdress) {
 		this.billingAdress = billingAdress;
 	}
+
 	public String getPhoneNumber() {
 		return phoneNumber;
 	}
+
 	public void setPhoneNumber(String phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
+
 	public boolean isVipStatus() {
 		return vipStatus;
 	}
+
 	public void setVipStatus(boolean vipStatus) {
 		this.vipStatus = vipStatus;
 	}
-	
+
+	public String getNickName() {
+		return nickName;
+	}
+
+	public void setNickName(String nickname) {
+		this.nickName = nickname;
+	}
+
 	@Override
     //<editor-fold defaultstate="collapsed" desc="toString override">
     public String toString() {
@@ -77,6 +130,6 @@ public class User {
         }
         result.append("}");
 
-        return result.toString();
-    }
+		return result.toString();
+	}
 }
