bfd629ff490ec96263402cfb8818e65c6b4353b7
Pancaldi Marta (Student Com14) <marta.pancaldi@stud-inf.unibz.it>
2016-12-15 17:09:12 +0100

    Tables stuff Temporarely commented in WebappListener, DB connections

---
+++ src/wpb/SeedDB.java
@@ -0,0 +1,119 @@
+package wpb;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import wpb.item.Item;
+import wpb.item.ItemManager;
+import wpb.reservation.Reservation;
+import wpb.roomtable.RoomTable;
+import wpb.roomtable.RoomTable.CategoryType;
+import wpb.tableorder.TableOrder;
+import wpb.tableorder.TableOrderManager;
+
+/**
+ *
+ * @author Werner
+ */
+public class SeedDB {
+
+	private static Connection connection = null;
+	private static SessionFactory mySessionFactory = null;
+	
+	public static void setSessionFactory(SessionFactory sf) {
+		mySessionFactory = sf;
+	}
+	
+	@Deprecated
+	public static void testDBConnection() {
+		try {
+			if (connection == null) {
+				// jdbc:postgresql://qdjjtnkv.db.elephantsql.com:5432/nqjahvby
+				String host = "localhost";
+				String database = "RAIBZ";
+				String username = "raitest";
+				String password = "raitest";
+				String url = "jdbc:postgresql://" + host + "/" + database;
+				String driverJDBC = "org.postgresql.Driver";
+				Class.forName(driverJDBC);
+				connection = DriverManager.getConnection(url, username, password);
+				// line firing the class not found exception
+
+			} else if (connection.isClosed()) {
+				connection = null;
+				// connect();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace(System.err);
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+		}
+	}
+
+	public static void seedItems(int count){
+		
+		ItemManager itmManager = new ItemManager(mySessionFactory);
+		SessionIdentifierGenerator idgen= new SessionIdentifierGenerator();
+		Random R = new Random();
+		
+		//creating random items
+		for (int i = 1; i <= count; i++) {
+			Item newItem = new Item();
+			newItem.setAvailable(R.nextBoolean());
+			newItem.setPrice(Math.floor(ThreadLocalRandom.current().nextDouble(0.1, 12) * 100) / 100);
+			newItem.setName(idgen.nextSessionId());
+			itmManager.addItem(newItem);
+		}
+		
+		System.out.println(itmManager.getTotalCount() + " new Items created");
+	}
+	
+public static void seedTableOrders(int count){
+		
+		TableOrderManager itmManager = new TableOrderManager(mySessionFactory);
+		SessionIdentifierGenerator idgen= new SessionIdentifierGenerator();
+		Random R = new Random();
+		
+		//creating random items
+		for (int i = 1; i <= count; i++) {
+			TableOrder newItem = new TableOrder();
+			newItem.setStatus(idgen.nextSessionId());
+			itmManager.addTableOrder(newItem);
+		}
+		
+		System.out.println(itmManager.getTotalCount() + " new Items created");
+	}
+	
+	public static void seedRoomTables(int count){
+		
+		/*
+		TabManager tableManager = new ItemManager(mySessionFactory);
+		SessionIdentifierGenerator idgen= new SessionIdentifierGenerator();
+		
+		//creating random Tables
+		for (int i = 1; i <= count; i++) {
+			RoomTable newTable = new RoomTable();
+			newItem.setAvailable(true);
+			newItem.setPrice(Math.floor(ThreadLocalRandom.current().nextDouble(0.1, 12) * 100) / 100);
+			newItem.setName(idgen.nextSessionId());
+			itmManager.addItem(newItem);
+		}
+		
+		System.out.println(itmManager.getTotalCount() + " new Items created");
+		*/
+	}
+	
+}
---
+++ src/wpb/WebAppListener.java
@@ -0,0 +1,28 @@
+package wpb;
+
+import javax.servlet.ServletContextEvent;  
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+import org.hibernate.SessionFactory;
+
+@WebListener
+public class WebAppListener implements ServletContextListener {
+
+	
+	    @Override
+	    public void contextInitialized(ServletContextEvent servletContextEvent) {
+	        
+	    	System.out.println("Start up!");
+	    	SessionFactory mySessionFactory = HibernateUtil.getSessionJavaConfigFactory();
+	        SeedDB.setSessionFactory(mySessionFactory);
+	        SeedDB.seedItems(200);
+	        //SeedDB.seedTableOrders(5);
+	    }
+
+	    @Override
+	    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+	        System.out.println("Shutting down!");
+	    }
+	}
+
