602e4a53b990e4e994fbf9a3096ca4010c4d2a0b
wsperandio <Werner.Sperandio@stud-inf.unibz.it>
2017-01-11 13:39:22 +0100

    add TimeStampUtil class for ISO8601

---
+++ src/wpb/util/TimestampUtils.java
@@ -0,0 +1,26 @@
+package wpb.util;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+/**
+ * Methods for dealing with timestamps
+ */
+public class TimestampUtils {
+
+	/**
+	 * Return an ISO 8601 combined date and time string for specified date/time
+	 * 
+	 * @param date
+	 *            Date
+	 * @return String with format "yyyy-MM-dd'T'HH:mm:ss'Z'"
+	 */
+	public static String getISO8601String(Date date) {
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.US);
+		dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+		return dateFormat.format(date);
+	}
+}
